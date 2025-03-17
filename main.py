3446d4106539b76c172c5e958e9e7abec75f2c13
estudanteJoaoElias <joao.fogaca0508@gmail.com>
2025-03-16 22:33:12 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,21 @@
+#numeros inteiros
+idade = 10
+print(type(idade))
+
+#Ponto flutuante
+altura = 175.5
+print(type(altura))
+
+#String
+nome = "ana"
+print(type(nome))
+
+#Lista
+lista = [5, 10, 20, 30]
+print(type(lista))
+
+#booleano
+verdadeiro = True
+falso = False
+print(type(verdadeiro))
+print(type(falso))
